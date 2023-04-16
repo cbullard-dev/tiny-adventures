d44fdf1633e109f0b7eceb9eb85de04d6704951c
Cal <cal@arcticstagmedia.com>
2023-04-17 08:48:27 +1000

    Hard coded the camera follow to get the current x and y possition of the Player character and then set the camerat x and y to the player positionas long as the player possition is not less than the min x and y possition

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,23 +8,27 @@ public class CameraFollow : MonoBehaviour
   [SerializeField] private Transform playerPosition;
   [SerializeField] private Vector3 MovementZone;
   [SerializeField]
-  [Min(0)]
-  private float xMinPoint = 0;
+  [Min(-100)]
+  private float xMinPoint = -20;
 
   private float yMinPoint = 6;
 
 
+  void awake()
+  {
+    // playerPosition = GameObject.FindWithTag("Player").GetComponent<Transform>();
+  }
 
   // Start is called before the first frame update
   void Start()
   {
-
+    this.transform.position = new Vector3(playerPosition.position.x,playerPosition.position.y > yMinPoint ? playerPosition.position.y : yMinPoint,this.transform.position.z);
   }
 
   // Update is called once per frame
   void Update()
   {
-
+    this.transform.position = new Vector3(playerPosition.position.x > xMinPoint ? playerPosition.position.x : xMinPoint,playerPosition.position.y > yMinPoint ? playerPosition.position.y : yMinPoint,this.transform.position.z);
   }
 
   private void OnDrawGizmos()
