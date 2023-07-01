9216a4bd6dcf642c96fea8f8156bc48ddcae54f3
Cal <cal@arcticstagmedia.com>
2023-07-02 09:54:30 +1000

    Updated the pause menu with functional controls Added controller support to pause menu

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
 public class GameUIManager : MonoBehaviour
 {
   [SerializeField] private TextMeshProUGUI scoreText;
   [SerializeField] private Image healthBar;
-  [SerializeField] private GameObject PauseMenu;
+  [SerializeField] private GameObject PauseMenu, resumeButton;
   // Start is called before the first frame update
   void Start()
   {
@@ -38,11 +39,18 @@ public class GameUIManager : MonoBehaviour
     if (GameManager.Instance.isPaused)
     {
       PauseMenu.SetActive(true);
+      SetMenuInput(resumeButton);
     }
 
     if (!GameManager.Instance.isPaused)
     {
       PauseMenu.SetActive(false);
     }
+  }
+
+  private void SetMenuInput(GameObject buttonObject)
+  {
+    EventSystem.current.firstSelectedGameObject = null;
+    EventSystem.current.firstSelectedGameObject = buttonObject;
   }
 }
