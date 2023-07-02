17d4de346854c453112e0f7ac05d4cc45e39883b
Cal <cal@arcticstagmedia.com>
2023-07-02 17:14:45 +1000

    Fixed a bug that caused player score to never reset between runs Updated the OSX Standalone build settings Updated the project setting to set max resolution to 1920 x 1080

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,6 +14,7 @@ public class MainMenu : MonoBehaviour
   public void PlayGame()
   {
     GameManager.Instance.GameOver = false;
+    GameManager.Instance.PlayerScore = 0;
     GameManager.Instance.LoadLevel(2);
   }
 }
