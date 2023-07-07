abfb0f3fed9a61aed66e474c268910f58acd3bfd
Cal <cal@arcticstagmedia.com>
2023-07-07 12:34:12 +1000

    Updated the existing checkpoint element with the new checkpoint flag

--- Assets/_Scripts/CheckpointController.cs
+++ Assets/_Scripts/CheckpointController.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class CheckpointController : MonoBehaviour
 {
 
-  private bool activated = false;
+  [SerializeField] private Animator _animator;
+  private bool _activated = false;
+  private static readonly int Activeated = Animator.StringToHash("_activated");
 
   private void OnTriggerEnter2D(Collider2D other)
   {
-    if (other.gameObject.tag != "Player") return;
-    if (activated) return;
-    activated = true;
+    if (!other.gameObject.CompareTag("Player")) return;
+    if (_activated) return;
+    _activated = true;
+    _animator.SetBool(Activeated,_activated);
     GameObject respawn = GameObject.FindWithTag("Respawn");
     if (!respawn) return;
     respawn.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 1.5f, this.transform.position.z);
